da6897e849216df1cd345fd4a4b9da74804eb498
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-10-21 01:57:01 +0600

    Folder setup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const express = require("express");
 const app = express();
 
 const config = require("./config");
+const routes = require("./routes/api");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+app.use("/api", routes);
 
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
